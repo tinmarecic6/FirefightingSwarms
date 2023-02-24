6bc8e1cd52204c97bf22acb41831b08d95083ab0
DjStonie <valdonay@yahoo.dk>
2023-02-24 11:50:52 +0100

    up

--- TestRobot/controllers/RobotController/RobotController.py
+++ TestRobot/controllers/RobotController/RobotController.py
@@ -14,20 +14,36 @@ for i in range(4):
     wheels.append(robot.getDevice(wheelsNames[i]))
     wheels[i].setPosition(float('inf'))
     wheels[i].setVelocity(0.0)
-avoidObstacleCounter = 0
-while robot.step(TIME_STEP) != -1:
-    leftSpeed = 1.0
-    rightSpeed = 1.0
-    if avoidObstacleCounter > 0:
-        avoidObstacleCounter -= 1
-        leftSpeed = 1.0
-        rightSpeed = -1.0
-    else:  # read sensors
-        for i in range(2):
-            if ds[i].getValue() < 950.0:
-                avoidObstacleCounter = 100
+
+def HandleLight(left, right):
+    leftSpeed = 0
+    rightSpeed = 0
+    if (left == 1000 and right == 1000):
+        leftSpeed = 0
+        rightSpeed = 0
+    elif (left == 1000):
+        leftSpeed = 10
+        rightSpeed = -5
+    elif (right == 1000):
+        leftSpeed = -5
+        rightSpeed = 10
+    elif (left>right):
+        leftSpeed = 10
+        rightSpeed = 5
+    elif (right>left):
+        leftSpeed = 5
+        rightSpeed = 10
     wheels[0].setVelocity(leftSpeed)
     wheels[1].setVelocity(rightSpeed)
     wheels[2].setVelocity(leftSpeed)
     wheels[3].setVelocity(rightSpeed)
+    
+while robot.step(TIME_STEP) != -1:
+    leftSensor = ds[0].getValue()
+    rightSensor = ds[1].getValue()
+    print("left: "+str(leftSensor))
+    print("right: "+str(rightSensor))
+    HandleLight(leftSensor, rightSensor)
+    
+    
 
