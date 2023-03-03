07bef266845742cfe349300b288d1423902b0108
tmarecic <tin.marecic@gmail.com>
2023-03-03 11:01:44 +0100

    Wip

---
+++ 2d_Test/controllers/shared_controler/shared_controler.py
@@ -0,0 +1,16 @@
+"""my_controller controller."""
+
+# You may need to import some classes of the controller module. Ex:
+#  from controller import Robot, Motor, DistanceSensor
+from controller import Robot
+import 
+
+
+
+		
+
+# Enter here exit cleanup code.
+if __name__ == "__main__":    
+	# create the Robot instance.
+	robot = Robot()
+	run_robot(robot)
